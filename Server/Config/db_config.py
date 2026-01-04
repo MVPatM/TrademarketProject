788e89297d1d797fb23f3d3ede89e9141ef79b2d
MVPatM <76089856+MVPatM@users.noreply.github.com>
2026-01-04 10:57:23 +0900

    Update and rename Server/db_config.py to Server/Config/db_config.py

--- Server/Config/db_config.py
+++ Server/Config/db_config.py
@@ -1,5 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 engine = create_engine("mysql+pymysql://root:1234@localhost:3306/test", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@contextmanager
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
